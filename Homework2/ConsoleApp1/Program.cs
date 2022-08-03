fa268fd34c1f99f7db4849c2d5a71d0c43efbbf9
Pavel-Levanok <105275978+Pavel-Levanok@users.noreply.github.com>
2022-08-03 17:00:31 +0300

    Homework2_factorial completed

---
+++ Homework2/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            //Вычислить факториал числа, которое ввел пользователь.
+        {
+            Console.WriteLine("Enter your number");
+            int a = int.Parse(Console.ReadLine());
+            int f = 1;
+            for (int i = 1; i <= a; i++)
+            {
+                f = f * i;
+            }
+            Console.WriteLine($"Factorial is {f}");
+            Console.ReadKey();
+        }
+    }
+}
