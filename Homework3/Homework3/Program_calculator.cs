39d5f33adc7bd5be16fd43bf75b44dfbc7db5f47
Pavel-Levanok <105275978+Pavel-Levanok@users.noreply.github.com>
2022-08-03 16:56:41 +0300

    Homework3_ABC completed

---
+++ Homework3/Homework3/Program_calculator.cs
@@ -0,0 +1,36 @@
+﻿namespace Homework3
+{
+    internal class Calculator
+        //Сделать консольный калькулятор.Пользователь вводит два целых числа и операцию(+,-,/,)
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CALCULATOR");
+            Console.WriteLine("PLease, Enter your 2 numbers");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your operetion: +,-,/");
+            string operation = Console.ReadLine();
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine($"Your Result:{a + b}");
+                    break;
+
+                case "-":
+                    Console.WriteLine($"Your Result:{a - b}");
+                    break;
+
+                case "/":
+                    Console.WriteLine($"Your Result:{a / b}");
+                    break;
+
+                default:
+                    Console.WriteLine("ERROR");
+                    break;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
