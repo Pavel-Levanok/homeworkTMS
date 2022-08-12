502dd6b5878768976c11151859dd1d5ebba19646
Pavel-Levanok <105275978+Pavel-Levanok@users.noreply.github.com>
2022-08-12 12:25:01 +0300

    Homework6_phones

--- Homework6/Homework6_phone/Homework6_phone/Program.cs
+++ Homework6/Homework6_phone/Homework6_phone/Program.cs
@@ -4,27 +4,25 @@
     {
         static void Main(string[] args)
         {
-            //Phone phone1 = new Phone("452685", "Samsung", 200d);
-            //Phone phone2 = new Phone("032432", "Nokia", 180d);
-            //Phone phone3 = new Phone("747775", "LG", -200d);
-            //Phone phone4 = new Phone("644654", "Sony", 170d);
-            Phone[] phones = new Phone[] { phone2, phone3, phone4 };
-            List<Phone> phones = new List<Phone>()
-            {
-                new Phone("452685", "Samsung", 200d), phone2, phone3, phone4
-            };
+            Phone phone1 = new Phone("452685", "Samsung", 200d);
+            Phone phone2 = new Phone("032432", "Nokia", 180d);
+            Phone phone3 = new Phone("747775", "LG", -200d);
+            Phone phone4 = new Phone("644654", "Sony", 170d);
+            //Phone[] phones = new Phone[] { phone2, phone3, phone4 };
+            //List<Phone> phones = new List<Phone>()
+            //{
+            //    new Phone("452685", "Samsung", 200d), phone2, phone3, phone4
+            //};
 
-            foreach (Phone phone in phones)
-            {
-                Console.WriteLine($"1. Номер:{phone.Number}, модель:{phone.Model}, вес: {phone.Weight} гр");
-            }
+            //foreach (Phone phone in phones)
+            //{
+            //    Console.WriteLine($"1. Номер:{phone.Number}, модель:{phone.Model}, вес: {phone.Weight} гр");
+            //}
             Console.WriteLine($"1. Номер:{phone1.Number}, модель:{phone1.Model}, вес: {phone1.Weight} гр");
             Console.WriteLine($"2. Номер:{phone2.Number}, модель:{phone2.Model}, вес: {phone2.Weight} гр");
             Console.WriteLine($"3. Номер:{phone3.Number}, модель:{phone3.Model}, вес: {phone3.Weight} гр");
             Console.WriteLine($"4. Номер:{phone4.Number}, модель:{phone4.Model}, вес: {phone4.Weight} гр");
             
-            Console.WriteLine(phone3.Weight);
-
             phone1.RecieveCall("Вася");
             phone2.RecieveCall("Денис");
         }
