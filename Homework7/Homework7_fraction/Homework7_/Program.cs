35323ac9313184e0dccdcfd5da6ba8cf9ff367b2
Pavel-Levanok <105275978+Pavel-Levanok@users.noreply.github.com>
2022-08-23 01:25:16 +0300

    Homework_fraction ready

---
+++ Homework7/Homework7_fraction/Homework7_/Program.cs
@@ -0,0 +1,104 @@
+﻿namespace Homework7_fraction
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Fraction fraction1 = new Fraction(3, -27);
+            Fraction fraction2 = new Fraction(6, 24);
+
+            Console.WriteLine("Есть две следующие дроби:");
+            Fraction.Print(fraction1);
+            Fraction.Print(fraction2);
+
+
+            if ((fraction1.Denominator == 0) || (fraction2.Denominator == 0)) 
+                    {
+                Console.WriteLine("Ошибка! Знаменатель равен 0. Введите другие дроби");
+            }
+
+            if ((fraction1.Denominator < 0) && (fraction1.Nominator < 0))
+            {
+                fraction1.Denominator = -1 * fraction1.Denominator;
+                fraction1.Nominator = -1 * fraction1.Nominator;
+            }
+
+            if ((fraction2.Denominator < 0) && (fraction2.Nominator < 0))
+            {
+                fraction2.Denominator = -1 * fraction2.Denominator;
+                fraction2.Nominator = -1 * fraction2.Nominator;
+            }
+
+            if (fraction1.Denominator < 0) 
+            {
+                fraction1.Denominator = -1 * fraction1.Denominator;
+                fraction1.Nominator = -1 * fraction1.Nominator;
+            }
+
+            if (fraction2.Denominator < 0)
+            {
+                fraction2.Denominator = -1 * fraction2.Denominator;
+                fraction2.Nominator = -1 * fraction2.Nominator;
+            }
+            
+            int nod1 = fraction1.Nod(fraction1);
+            if (nod1 != 0)
+            {
+                fraction1.Nominator = fraction1.Nominator / nod1;
+                fraction1.Denominator = fraction1.Denominator / nod1;
+            }
+            
+            int nod2 = fraction2.Nod(fraction2);
+            if (nod2 != 0)
+            {
+                fraction2.Nominator = fraction2.Nominator / nod2;
+                fraction2.Denominator = fraction2.Denominator / nod2;
+            }
+            if ((nod1 != 0) || (nod2 != 0))
+            {
+                Console.WriteLine("Ваши дроби после сокращения и привидения их в нормальный вид:");
+                Fraction.Print(fraction1);
+                Fraction.Print(fraction2);
+            }
+
+            Fraction Sum = Fraction.Sum(fraction1, fraction2);
+            int nod3 = Sum.Nod(Sum);
+            if (nod3 != 0)
+            {
+                Sum.Nominator = Sum.Nominator / nod3;
+                Sum.Denominator = Sum.Denominator / nod3;
+            }
+            Console.WriteLine("Результат СУММЫ дробей:");
+            Fraction.Print(Sum);
+
+            Fraction Subtraction = Fraction.Subtraction(fraction1, fraction2);
+            int nod4 = Subtraction.Nod(Subtraction);
+            if (nod4 != 0)
+            {
+                Subtraction.Nominator = Subtraction.Nominator / nod4;
+                Subtraction.Denominator = Subtraction.Denominator / nod4;
+            }
+            Console.WriteLine("Результат ВЫЧИТАНИЯ дробей:");
+            Fraction.Print(Subtraction);
+
+            Console.WriteLine("Результат УМНОЖЕНИЯ дробей:");                   
+            Fraction.Print(Fraction.Multiplication(fraction1, fraction2));
+                      
+
+            Console.WriteLine("Результат ДЕЛЕНИЯ дробей:");
+            Fraction.Print(Fraction.Division(fraction1, fraction2));
+
+            
+            Console.WriteLine("Значения дробей в формате double:");
+            Console.WriteLine(fraction1.toDouble(fraction1));
+            Console.WriteLine(fraction2.toDouble(fraction2));
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Homework7/Homework7_fraction/Homework7_/fraction.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework7_fraction
+{
+    internal class Fraction
+    {
+        //public int _nominator;
+        //public int _denominator;
+
+        public Fraction(int nominator, int denominator)
+        {
+
+            Nominator = nominator;
+            Denominator = denominator;
+        }
+
+        public int Nominator { get; set; }
+        public int Denominator { get; set; }
+
+        internal static void Print(Fraction fraction)
+        {
+            Console.WriteLine($"{fraction.Nominator}/{fraction.Denominator}");
+
+        }
+
+        public int Nod(Fraction fraction)
+        {
+            int nominator = fraction.Nominator;
+            int denominator = fraction.Denominator;
+            nominator = Math.Abs(nominator);
+            denominator = Math.Abs(denominator);
+            while (denominator != 0 && nominator != 0)
+            {
+                if (nominator % denominator > 0)
+                {
+                    var temp = nominator;
+                    nominator = denominator;
+                    denominator = temp % denominator;
+                }
+                else break;
+            }
+            if (denominator != 0 && nominator != 0) return denominator;
+            return 0;
+        }
+
+        internal static Fraction Sum(Fraction fraction1, Fraction fraction2)
+        {
+            Fraction result_Sum = new Fraction(fraction1.Nominator * fraction2.Denominator + fraction2.Nominator * fraction1.Denominator, fraction1.Denominator * fraction2.Denominator);
+            return result_Sum;
+        }
+
+        internal static Fraction Subtraction(Fraction fraction1, Fraction fraction2)
+        {
+            Fraction result_Subtraction = new Fraction(fraction1.Nominator * fraction2.Denominator - fraction2.Nominator * fraction1.Denominator, fraction1.Denominator * fraction2.Denominator);
+            return result_Subtraction;
+        }
+
+
+
+        internal static Fraction Multiplication(Fraction fraction1, Fraction fraction2)
+        {
+            Fraction result_Multiplication = new Fraction(fraction1.Nominator * fraction2.Nominator, fraction1.Denominator * fraction2.Denominator);
+            if (result_Multiplication.Denominator < 0)
+            {
+                result_Multiplication.Denominator = -1 * result_Multiplication.Denominator;
+                result_Multiplication.Nominator = -1 * result_Multiplication.Nominator;
+            }
+            return result_Multiplication;
+        }
+
+
+        internal static Fraction Division(Fraction fraction1, Fraction fraction2)
+        {
+            Fraction result_division = new Fraction(fraction1.Nominator * fraction2.Denominator, fraction1.Denominator * fraction2.Nominator);
+            if (result_division.Denominator < 0)
+            {
+                result_division.Denominator = -1 * result_division.Denominator;
+                result_division.Nominator = -1 * result_division.Nominator;
+            }
+            return result_division;
+        }
+
+        public double toDouble(Fraction fraction)
+        {
+            double result = (double)fraction.Nominator / (double)fraction.Denominator;
+            return result;
+
+        }
+
+
+
+
+    }
+}
