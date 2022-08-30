37d6b409d0296aaf1f54c9858ef1b6dc6e07009b
Pavel-Levanok <105275978+Pavel-Levanok@users.noreply.github.com>
2022-08-31 02:12:56 +0300

    Homework10_microwave completed

---
+++ Homework10/Homework10/Microwave.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework10
+{
+    public delegate void NotifyWarmUpCompleted(string dish);
+    internal class Microwave
+    {
+        public event NotifyWarmUpCompleted WarmUpCompleted;
+        
+        public void WarmUp(string dish)
+        {
+            Console.WriteLine($"Подогреваю {dish}");
+            WarmUpCompleted?.Invoke(dish);
+        }
+    }
+}
---
+++ Homework10/Homework10/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Homework10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Microwave microwave1 = new Microwave();
+            microwave1.WarmUpCompleted += WarmUpCompletedHandler;
+            microwave1.WarmUp("Хот Дог");
+            microwave1.WarmUp("Пицца");
+        }
+        public static void WarmUpCompletedHandler(string dish)
+        {
+            Console.WriteLine($"Ваша еда готова: {dish}");
+        }
+    }
+}
