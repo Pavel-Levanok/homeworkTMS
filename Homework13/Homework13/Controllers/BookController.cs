80e6b9ea45c31f2d8e61e4e762457bddd24233d0
Pavel-Levanok <105275978+Pavel-Levanok@users.noreply.github.com>
2022-09-02 18:24:16 +0300

    Homework13 Web aplication MCV

---
+++ Homework13/Homework13/Controllers/BookController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Homework13.Models;
+
+namespace Homework13.Controllers
+{
+    
+        public class BookController : Controller
+        {
+            public IActionResult Index()
+            {
+                BookViewModel book = new BookViewModel("Сказки", "Пушкин", 241);
+                return View(book);
+            }
+        }
+    
+}
---
+++ Homework13/Homework13/Models/BookViewModel.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Homework13.Models
+{
+    public class BookViewModel
+    {
+        public BookViewModel(string name, string author, int pages)
+        {
+            Name = name;
+            Author = author;
+            Pages = pages;
+        }
+
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+    }
+}
+
