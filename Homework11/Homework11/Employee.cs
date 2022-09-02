1e83f1254f6179a1bacc1ff33e7a18e8472e3a6e
Pavel-Levanok <105275978+Pavel-Levanok@users.noreply.github.com>
2022-09-02 11:04:24 +0300

    Homework11_exception completed

---
+++ Homework11/Homework11/Employee.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework11
+{
+    internal class Employee
+    {
+        private int _age;
+        private string _name;
+        private double _salary;
+
+        
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value < 18)
+                {
+                    throw new ArgumentException("Ошибка! Ваш возраст менее 18 лет.");
+                }
+
+                else
+                {
+                    _age = value;
+                }
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if ((value == null) || (value == ""))  
+                {
+                    throw new ArgumentNullException ("Ошибка! Имя не может быть пустым");
+                }
+                else
+                {
+                    if (value.Length >= 100)
+                    {
+                        throw new ArgumentException("Ошибка! Строка не может быть более 100 символов");
+                    }
+                    else
+                    {
+                        _name = value;
+                    }                    
+                }
+            }
+        }
+        public double Salary
+        {
+            get
+            {
+                return _salary;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Ошибка! Зарплата не может быть меньше или равной 0");
+                }
+                else
+                {
+                    _salary = value;
+                }
+            }
+        }
+
+    }
+}
---
+++ Homework11/Homework11/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Homework11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Employee employee = new Employee();
+
+                Console.WriteLine("Введите имя:");
+                employee.Name = Console.ReadLine();
+                Console.WriteLine("Введите возраст:");
+                employee.Age = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите зарплату:");
+                employee.Salary = double.Parse(Console.ReadLine());
+                
+            }
+
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            finally
+            {
+                Console.WriteLine("finally");
+                
+            }
+
+        }
+    }
+}
